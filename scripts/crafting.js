286cc28f61dbba2c6a16145a7614aa4d7a89e57d
cadowtin <chasdowtin@gmail.com>
2023-11-13 01:08:26 -0600

    Add Duration to project title

--- scripts/crafting.js
+++ scripts/crafting.js
@@ -209,7 +209,7 @@ export async function craftAProject(crafterActor, itemDetails, skipDialog = true
  * @param {ActorPF2e} details.projectOwner The actor who owns the project. Usually the same as the crafterActor, but not always! 
  */
 async function rollCraftAProject(crafterActor, project, details) {
-    const actionName = localise("CraftWindow.Title");
+    const actionName = localise("CraftWindow.Title", { length: details.duration.toLowerCase().replace(/\b\w/g, s => s.toUpperCase())});
     let skillName = "crafting";
     const settingsRollOptions = [];
 
